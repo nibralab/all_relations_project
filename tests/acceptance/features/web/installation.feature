63461cec4c10e1f17342d8f6c2798f6c320baca0
Niels Braczek <nbraczek@bsds.de>
2017-05-20 23:53:02 +0200

    Update

--- tests/acceptance/features/web/installation.feature
+++ tests/acceptance/features/web/installation.feature
@@ -4,6 +4,6 @@ Feature: Installation
   I need to be able to install the component
 
   Scenario: Installing component on latest Joomla
-    Given there is a fresh instance of "latest Joomla" with database "MySQL 5.5"
+    Given I am logged in as "administrator"
     When I install the extension "pkg_all_relations_project"
     Then I should see "Test Project with Relations" in the "Components" menu
--- tests/features/bootstrap/WebContext.php
+++ tests/features/bootstrap/WebContext.php
@@ -20,59 +20,11 @@ class WebContext implements Context
 	}
 
 	/**
-	 * @Given there is a fresh instance of :arg1 with database :arg2
+	 * @Given I am logged in as :arg1
 	 */
-	public function thereIsAFreshInstanceOfWithDatabase($arg1, $arg2)
+	public function iAmLoggedInAs($arg1)
 	{
-		$joomla   = $this->splitVersion($arg1);
-		$database = $this->splitVersion($arg2);
-
-		echo "Checking if " . implode(':', $joomla) . " can be installed with " . implode(':', $database);
-
-		$this->validateApp($joomla, ['joomla']);
-		$this->validateApp($database, ['mysql', 'postresql']);
-
 		throw new PendingException();
-	}
-
-	/**
-	 * @param $arg
-	 *
-	 * @return array
-	 */
-	protected function splitVersion($arg)
-	{
-		$app = explode(' ', $arg, 2);
-
-		if (count($app) == 1)
-		{
-			$app[1] = 'latest';
-		}
-
-		if ($app[0] == 'latest')
-		{
-			$app = array_reverse($app);
-		}
-
-		array_walk($app, function (&$item)
-		{
-			$item = strtolower($item);
-		});
-
-		return $app;
-	}
-
-	/**
-	 * @param $app
-	 * @param $supported
-	 */
-	protected function validateApp($app, $supported)
-	{
-		if (!in_array($app[0], $supported))
-		{
-			$appList = implode(', ', $supported);
-			throw new UnexpectedValueException("{$app[0]} is not supported. Use one of [{$appList}]");
-		}
 	}
 
 	/**
