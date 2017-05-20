91d541e7ce77a23ad037fc9b6296f0bb22fbb1c4
Niels Braczek <nbraczek@bsds.de>
2017-05-20 15:09:38 +0200

    Initial commit

---
+++ src/administrator/components/com_all_relations_project/all_relations_project.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Test Project with Relations Admin Entry Point
+ *
+ * PHP version 5.3
+ *
+ * @version    1.0.0
+ * @package    AllRelationsProject
+ * @author     Niels Braczek <nbraczek@bsds.de>
+ * @copyright  Copyright (C)2017 BSDS Braczek Software- und DatenSysteme. All rights reserved.
+ * @license    MIT
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+echo "Hello World! Administration";
---
+++ src/components/com_all_relations_project/all_relations_project.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test Project with Relations Frontend Entry Point
+ *
+ * PHP version 5.3
+ *
+ * @version    1.0.0
+ * @package    AllRelationsProject
+ * @author     Niels Braczek <nbraczek@bsds.de>
+ * @copyright  Copyright (C)2017 BSDS Braczek Software- und DatenSysteme. All rights reserved.
+ * @license    MIT
+ */
+
+defined('_JEXEC') or die;
+
+echo "Hello World!";
